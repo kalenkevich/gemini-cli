db560311ad2758af34f1620db3e1c451ca3d7f89
Sandy Tao <sandytao520@icloud.com>
2026-03-31 13:31:22 -0700

    fix(config): disable JIT context loading by default (#24364)

--- packages/core/src/config/config.ts
+++ packages/core/src/config/config.ts
@@ -1153,7 +1153,7 @@ export class Config implements McpContext, AgentLoopContext {
       modelConfigServiceConfig ?? DEFAULT_MODEL_CONFIGS,
     );
 
-    this.experimentalJitContext = params.experimentalJitContext ?? true;
+    this.experimentalJitContext = params.experimentalJitContext ?? false;
     this.experimentalMemoryManager = params.experimentalMemoryManager ?? false;
     this.useAgentProtocol = params.useAgentProtocol ?? false;
     this.memoryBoundaryMarkers = params.memoryBoundaryMarkers ?? ['.git'];
